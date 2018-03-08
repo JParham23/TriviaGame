2107c87720ba5605a210ad744268e7abe6499891
Jamar Parham <Jamar720@gmail.com>
2018-03-08 10:38:53 -0500

    changes made

---
+++ index/assets/javascript/app.js
@@ -0,0 +1,162 @@
+var triviaQuestions = [{
+	question: "What is University of Florida's mascot?",
+	answerList: ["gators", "cats", "ducks", "bears"],
+	answer: 0
+},{
+	question: "What is Georgia Tech University's mascot?",
+	answerList: ["spiders", "bees", "sharks", "cats"],
+	answer: 1
+},{
+	question: "What is Georgia University's mascot",
+	answerList: ["bobcats", "huskies", "bull dogs", "sharks"],
+	answer: 2
+},{
+	question: "What is Oregon University's mascot?",
+	answerList: ["hornets", "ducks", "parrots", "cows"],
+	answer: 1
+},{
+	question: "What is Lousiana state university's mascot?",
+	answerList: ["camels", "dragons", "tigers", "sharks"],
+	answer: 2
+},{
+	question: "What is Georgetown University mascot",
+	answerList: ["hampsters", "indians", "lions", "bulldogs"],
+	answer: 3
+},{
+	question: "what is the mascot o University of Richmond?",
+	answerList: ["spiders", "squirrles", "ducks", "birds"],
+	answer: 0
+},{
+	question: "What is Oklahoma states Unicerstiy's mascot",
+	answerList: ["bulldog", "sooners", "spiders", "hawks"],
+	answer: 1
+},{
+	question: "What is Old Dominion Unviersity's mascot?",
+	answerList: ["monarch", "flies", "eagle", "bull"],
+	answer: 0
+},{
+	question: "What is Norfolk state university's mascot?",
+	answerList: ["tiger", "tomcat", "Spartan", "Cowboy"],
+	answer: 2
+},{
+	
+}];
+
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
